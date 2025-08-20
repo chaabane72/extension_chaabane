5b7ce747d53f35dc8a6aa0584a76232559b9df3d
chaabane72 <chaabane72@yahoo.fr>
2025-08-20 06:01:38 +0200

    Mise à jour pxt.json, actionneurs.ts

--- actionneurs.ts
+++ actionneurs.ts
@@ -37,7 +37,7 @@ namespace actionneurs {
      * @param duree durée du clignotement en secondes, eg: 5
      * @param frequence nombre de clignotements par seconde, eg: 2
      */
-    //% block="💡 faire clignoter la LED sur %broche pendant %duree s à %frequence fois(s)/s"
+    //% block="💡 faire clignoter la LED sur %broche pendant %duree s à %frequence fois/s"
     //% broche.fieldEditor="gridpicker" broche.fieldOptions.columns=4
     //% duree.min=1 duree.max=60
     //% frequence.min=1 frequence.max=10
